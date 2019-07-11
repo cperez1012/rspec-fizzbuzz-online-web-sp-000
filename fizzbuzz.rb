5255d65fe082edb2d1589cb35c6c8034f9598263
Christian Perez <perezchristian1012@gmail.com>
2019-07-11 12:50:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-  puts "Fizz"
+  if (int % 3 == 0)
+  return "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    return "Buzz"
   elsif int % 15 == 0
-    puts "FizzBuzz"
-  else int % 4 == 0
-    puts "nil"
+    return "FizzBuzz"
+  else 
+    return "nil"
   end
 end
